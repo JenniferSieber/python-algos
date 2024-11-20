97eb48d3e0cbe373a0c0e444723255e1c91d0436
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-11-20 15:00:53 -0700

    Update easyAlgos.py

--- easyAlgos.py
+++ easyAlgos.py
@@ -31,3 +31,13 @@ def valid_anagram_03(s, t):
 print(valid_anagram_03('anagram', 'nagaram'))
 print(valid_anagram_03('rat', 'car'))
 
+######################
+# Replace Elements with Greatest Element on Right Side (LC1299)
+def replaceElements(arr):
+    rightMax = -1
+    for i in range(len(arr) - 1, -1, -1):
+        newMax = max(rightMax, arr[i])
+        arr[i] = rightMax
+        rightMax = newMax  
+    return arr
+print(replaceElements([17,18,5,4,6,1]))
