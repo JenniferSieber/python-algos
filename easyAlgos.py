7b61b690c476e67eb456cc26f991dc9a2b3f0993
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-11-21 13:37:33 -0700

    Update easyAlgos.py

--- easyAlgos.py
+++ easyAlgos.py
@@ -39,20 +39,12 @@ def validPalindrome(s):
             newStr += c.lower()
     # compare newStr to newStr reversed
     return newStr == newStr[::-1]
-
 print(validPalindrome("hannah"))
 print(validPalindrome("racecar"))
 print(validPalindrome("race a car"))
 print(validPalindrome("A man, a plan, a canal: Panama")) #amanaplanacanalpanama
 
-# solution 2 --- implement the above modules within code and not using extra memory O(1)
-# sliding window Pointer pattern
-# deal with spaces and non alphanumerics and convert to lower()
-# USE ASCII printable characters to determine if alphanumeric
-# Acceptable ASCII codes: --- create helper Function
-# A = 65 to Z = 90  || a= 97 to z=122 ||  0 = 48 to 9 = 57
-# create while loop  to skip spaces  while character isAlphaNumeric() increment left ++
-# Time: O(n) Space: O(1) no extra memory
+# solution 2 ---  sliding window Pointer pattern
 # alphanumeric helper function
 def alphaNum(c):
     return (ord('A') <= ord(c) <= ord('Z') or
@@ -60,10 +52,7 @@ def alphaNum(c):
             ord('0') <= ord(c) <= ord('9'))
 
 def isPalindrome(s):
-    # assign l/r pointers
     l, r = 0, len(s) - 1
-    
-    # iterate & move pointers validate
     while l < r:
         # validate alphanumeric while not out of bounds -increment/decrement
         while l < r and not alphaNum(s[l]):
@@ -75,7 +64,6 @@ def isPalindrome(s):
         # update l/r for increment/decrement
         l, r = l + 1, r - 1
     return True
-print("isPalindrome without modules")
 print(isPalindrome("hannah"))
 print(isPalindrome("racecar"))
 print(isPalindrome("race a car"))
